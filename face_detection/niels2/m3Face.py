26ceaf462b659d3f548b9c168786ad336d287ce9
malteerasmussen <malteerasmussen@gmail.com>
2019-10-15 13:03:38 +0200

    updated eye detetion in batch

--- face_detection/niels2/m3Face.py
+++ face_detection/niels2/m3Face.py
@@ -18,7 +18,7 @@ def findEyes(inputImPath):
     #print("face_landmarks_list type was", type(face_landmarks_list))
     # Create a PIL imagedraw object so we can draw on the picture
     pil_image = Image.fromarray(inputIm)
-    
+    eyeImages = []
     d = ImageDraw.Draw(pil_image)
    
     if (len(face_landmarks_list) == 0):
@@ -36,22 +36,25 @@ def findEyes(inputImPath):
     
         if left_eye[0][1] > left_eye[3][1]:
             # If tilted left - based on how it's seen on image
-            #print("found eyes")
+            print("tilted left")
             left = [left_eye[0][0]-mg,left_eye[2][1]-mg,left_eye[3][0]+mg,left_eye[5][1]+mg]
             right = [right_eye[0][0]-mg,right_eye[2][1]-mg,right_eye[3][0]+mg,right_eye[5][1]+mg]
             d.rectangle((left),width=0)
             d.rectangle((right),width=0)
-            return [np.asarray(pil_image.crop(left)),np.asarray(pil_image.crop(right))];
+            eyeImages.append(np.asarray(pil_image.crop(left)))
+            eyeImages.append(np.asarray(pil_image.crop(right)))
             #pil_image.crop(left).show()
             #pil_image.crop(right).show()
         else:
             # If tilted right
-            print("was tilted")
+            print("tilted right")
             left = [left_eye[0][0]-mg,left_eye[1][1]-mg,left_eye[3][0]+mg,left_eye[4][1]+mg]
             right = [right_eye[0][0]-mg,right_eye[1][1]-mg,right_eye[3][0]+mg,right_eye[4][1]+mg]
             d.rectangle((left),width=0)
             d.rectangle((right),width=0)
-            #return np.asarray(pil_image.crop(left));
-            return [np.asarray(pil_image.crop(left)),np.asarray(pil_image.crop(right))]
+            eyeImages.append(np.asarray(pil_image.crop(left)))
+            eyeImages.append(np.asarray(pil_image.crop(right)))
             #pil_image.crop(left).show()
             #pil_image.crop(right).show()
+    print('eyeImages',type(eyeImages),len(eyeImages))
+    return eyeImages;
