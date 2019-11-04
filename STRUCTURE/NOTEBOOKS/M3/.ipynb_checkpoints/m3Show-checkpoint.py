6d5e1cfda29bc1287be5cf2b01be6373d32fbb39
Tor Petersen <tor.arnth.petersen@gmail.com>
2019-11-04 10:53:49 +0100

    High_Pass

--- STRUCTURE/NOTEBOOKS/M3/.ipynb_checkpoints/m3Show-checkpoint.py
+++ STRUCTURE/NOTEBOOKS/M3/.ipynb_checkpoints/m3Show-checkpoint.py
@@ -91,6 +91,23 @@ def rgbToHSV(inputImg):
     result = plt.show()
     
     return (inputImg)
+    
+def h_Pass(inputImage):
+    
+    laplacian = cv.Laplacian(img,cv.CV_64F)
+    sobelx = cv.Sobel(img,cv.CV_64F,1,0,ksize=5)
+    sobely = cv.Sobel(img,cv.CV_64F,0,1,ksize=5)
+    
+    plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
+    plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
+    plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
+    plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+    
+    plt.show()
+    
+    return (inputImg)
  
 
 
