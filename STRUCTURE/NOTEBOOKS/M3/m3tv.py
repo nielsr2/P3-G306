343c11acea59fd539ef5eb75170ef21887b558ba
MikkelKPersson <43847844+MikkelKPersson@users.noreply.github.com>
2019-11-06 20:34:22 +0100

    Zhao complete

--- STRUCTURE/NOTEBOOKS/M3/m3tv.py
+++ STRUCTURE/NOTEBOOKS/M3/m3tv.py
@@ -2,18 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import skimage
+from skimage import img_as_ubyte
 from skimage import restoration
 from M3 import m3F as m3F
 
 
+def denoise_tv_chambolle(inputImg, weight, eps, n_iter_max, multichannel, show):
 
-def denoise_tv_chambolle(inputImg,weight,eps,n_iter_max,multichannel, show):
+    # documentation: https://scikit-image.org/docs/dev/api/skimage.restoration.html#skimage.restoration.denoise_tv_chambolle
 
-    tvImg = skimage.restoration.denoise_tv_chambolle(inputImg, weight=weight, eps=eps, n_iter_max=n_iter_max, multichannel=multichannel)
+    tvImg = skimage.restoration.denoise_tv_chambolle(inputImg, weight=weight, eps=eps, n_iter_max=n_iter_max,
+                                                     multichannel=multichannel)
     # standard params skimage.restoration.denoise_tv_chambolle(inputImg, weight=0.1, eps=0.0002, n_iter_max=200, multichannel=False)
-    tvImg = cv2.cvtColor(tvImg, cv2.COLOR_GRAY2BGR)
+    print(len(tvImg.shape))
 
-    if (show):
+    tvImg = img_as_ubyte(tvImg)
+    if show:
         m3F.imshow(tvImg, "Total variation")
 
     return tvImg
