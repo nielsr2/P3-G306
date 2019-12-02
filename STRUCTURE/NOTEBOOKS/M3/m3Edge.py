02f5ad9643e05451e342ba93dcf48c91d235c33c
Niels <Nielsr2@gmail.com>
2019-12-02 13:44:03 +0100

    made canny

--- STRUCTURE/NOTEBOOKS/M3/m3Edge.py
+++ STRUCTURE/NOTEBOOKS/M3/m3Edge.py
@@ -6,3 +6,10 @@ def sobel(inputImg, show=True):
     out = cv2.Sobel(inputImg, inputImg.depth(), 0, 1, 3)
     if show:
         m3Show.imshow(out, "show")
+
+
+def canny(inputImg, show=True, thresh1=100, thresh2=100):
+    out = cv2.Canny(inputImg, thresh1, thresh2)
+    if show:
+        m3Show.imshow(out, "canny")
+    return out
